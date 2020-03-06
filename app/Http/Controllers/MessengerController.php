d1aa58b300b7b83a58c05c3ab389c0d5c05c362b
Gaál Gergely <gaalgergely@yahoo.com>
2020-03-06 23:44:55 +0100

    Messenger is connected to the chatbot

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Log;
 use pimax\FbBotApp;
 use pimax\Messages\Message;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,5 +21,14 @@ class MessengerController extends Controller
         }
 
         return response()->json(['Bad verify token.'], Response::HTTP_FORBIDDEN);
+    }
+
+    public function webHookPost(Request $request)
+    {
+        //Log::info('Message received.' . print_r($request->all(), 1));
+
+        $bot = new FbBotApp(env('PAGE_ACCESS_TOKEN'));
+        $message = new Message($request->get('entry')[0]['messaging'][0]['sender']['id'], 'Hello beautiful world!');
+        $bot->send($message);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/webhook', 'MessengerController@webHook');
+Route::post('/webhook', 'MessengerController@webHookPost');
